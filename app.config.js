1ed849ed2d2c4cd1ab6e5723097957b61c38eb66
ulysse <ulysse@alenvi.io>
2025-04-14 18:46:59 +0200

    update version 2.33.0

--- app.config.js
+++ app.config.js
@@ -44,7 +44,7 @@ export default {
     slug: 'compani',
     description: 'Nous aidons les intervenants, les managers du secteur et les dirigeants à pratiquer un accompagnement humain',
     platforms: ['ios', 'android', 'web'],
-    version: '2.32.0',
+    version: '2.33.0',
     orientation: 'portrait',
     primaryColor: '#005774',
     icon: './assets/images/ios_icon.png',
@@ -71,7 +71,7 @@ export default {
       color: '#005774',
     },
     ios: {
-      buildNumber: '2.32.0.1',
+      buildNumber: '2.33.0',
       bundleIdentifier: variables.bundleIdentifier,
       requireFullScreen: true,
       icon: './assets/images/ios_icon.png',
@@ -112,7 +112,7 @@ export default {
         resizeMode: 'cover',
         backgroundColor: '#FFFFFF',
       },
-      versionCode: 260,
+      versionCode: 270,
     },
     web: {
       favicon: './assets/images/android_icon_old.png',
