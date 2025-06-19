1dba1008f2f50b44efddccd1266daeae055afa4d
ulysse <ulysse@alenvi.io>
2025-06-19 16:40:51 +0200

    COM-4009: update tsconfig

--- src/components/cards/FillTheGapPropositionList/index.tsx
+++ src/components/cards/FillTheGapPropositionList/index.tsx
@@ -1,3 +1,4 @@
+import { JSX } from 'react';
 import { View } from 'react-native';
 import { DraxView } from 'react-native-drax';
 import { FillTheGapAnswers } from '../../../screens/courses/cardTemplates/FillTheGapCard';
